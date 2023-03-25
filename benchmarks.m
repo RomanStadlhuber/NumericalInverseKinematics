32f92a5080a9a99b2f6738b936f271cf5eac268a
Roman Stadlhuber <roman.stadlhuber@gmail.com>
2023-03-25 20:02:29 +0100

    add benchmarking script compares LM to BFGS add trajectory trace function for BFGS

---
+++ benchmarks.m
@@ -0,0 +1,56 @@
+%% clear workspace and console
+close all;
+clear;
+clc;
+
+%% create robot
+
+
+% Denavit-Hartenberg parameters:
+%           A       alpha   d   theta
+dhparams = [0       0       0.333   0
+            0       -pi/2   0       0
+            0       pi/2    0.316   0
+            0.0825  pi/2    0       0
+            -0.0825 -pi/2   0.384   0
+            0       pi/2    0       0
+            0.088   pi/2    0       0];
+% NOTE: parameters obtained from
+% https://frankaemika.github.io/docs/control_parameters.html#denavithartenberg-parameters
+jTypes = ["revolute" "revolute" "revolute" "revolute" "revolute" "revolute" "revolute"];
+
+robot = getRigidBodyTree(dhparams, jTypes);
+
+tcpName = char(robot.BodyNames(robot.NumBodies));
+
+%% setup IK
+
+targetPositions = [ 0.17   0.12   0.05  -0.03  -0.12  -0.10  -0.14  -0.18  -0.15
+                    0.09   0.15   0.20   0.20   0.15   0.15   0.08  -0.10  -0.07
+                    0.05   0.10   0.08   0.14   0.20   0.16   0.07   0.04   0.20];
+
+targetPositions = 2.5 .* targetPositions;
+
+[~, numWaypoints] = size(targetPositions);
+waypoints = zeros(4, 4, numWaypoints);
+for idxWaypoint = 1:numWaypoints
+    waypoints(:,:, idxWaypoint) = trvec2tform(targetPositions(:,idxWaypoint).');
+end
+weights = [0 0 0 1 1 1];
+% initialGuess = monteCarloInitialGuess(robot, tcpName, waypoints(:,:,1));
+initialGuess = homeConfiguration(robot);
+minDistance = 1e-5;
+maxIterations = 150;
+% set to true to obtain diagnostic information in the workspace
+diagnosticMode = true;
+%% compute inverse kinematics for entire trajectory
+
+% LM
+[outTrajectory, outJointStates] = traceTrajectory(robot, tcpName, waypoints, maxIterations, minDistance, weights, initialGuess, diagnosticMode);
+% BGFS
+[outTrajectoryBFGS, outJointStatesBFGS] = traceTrajectoryBFGS(robot, tcpName, waypoints, maxIterations, minDistance, weights, initialGuess, diagnosticMode);
+
+
+
+%% plot trajectory
+viz(robot, outTrajectoryBFGS, targetPositions, outJointStatesBFGS);
---
+++ traceTrajectoryBFGS.m
@@ -0,0 +1,81 @@
+function [outTrajectory, articulations] = traceTrajectoryBFGS(robot, tcpName, inTrajectory, maxIterations, minDistance, weights, initialGuess, diagnostic)
+    % set a flag prompting the export of diagnostic information upon
+    % exiting the function
+    diagnosticMode = 0;
+    if exist("diagnostic", "var")
+        diagnosticMode = logical(diagnostic);
+    end
+    % set the weighting vector if not provided (defaults to all weights in full)
+    if ~exist('weights', 'var')
+        weights = [1 1 1 1 1 1];
+    end
+
+    % NOTE: assume the shape of the trajectory to be (4, 4, numWaypoints)
+    [~, ~, numWaypoints] = size(inTrajectory);
+    % number of joints (required to fill the articulation space)
+    numJoints = size(homeConfiguration(robot), 1);
+    % set initial articulation to be the home joint state of the robot
+    articulation = homeConfiguration(robot);
+    % set articulation to initial guess if provided
+    if exist('initialGuess', 'var')
+        articulation = initialGuess;
+    end
+    % weighting matrix
+    W = diag([weights(4:6), weights(1:3)]);
+    % initialize the output trajectory storage
+    outTrajectory = zeros([4 4 numWaypoints]);
+    articulations = zeros(numJoints, numWaypoints);
+    iterationsPerWaypoint = zeros([1 numWaypoints], "int32");
+    
+    for idxWaypoint = 1:numWaypoints
+    
+        % --- BFGS-IK ---
+        % setup the IK solver for BFGS Gradient Projection
+        % for "inverseKinematics" object, see: https://de.mathworks.com/help/robotics/ref/inversekinematics-system-object.html
+        % for function parameters, see: https://de.mathworks.com/help/robotics/ug/inverse-kinematics-algorithms.html#bve7ae8
+
+        solverParameters = struct( ...
+            "MaxIterations", maxIterations, ...
+            "SolutionTolerance", minDistance, ...
+            "AllowRandomRestarts", false);
+        bfgs_ik = inverseKinematics("RigidBodyTree", robot, ...
+                                    "SolverAlgorithm", "BFGSGradientProjection", ...
+                                    "SolverParameters", solverParameters);
+                                    
+        % desired transformation in space frame
+        T_sd = inTrajectory(:, :, idxWaypoint);
+        [articulation, solutionInfo] = bfgs_ik(tcpName, T_sd, weights, articulation);
+        numIterations = solutionInfo.Iterations;
+        % endeffector transformation in space frame
+        T_sb = getTransform(robot, articulation, tcpName);
+        currDistance = norm(W * adjointSE3(T_sb) * errorTwist(T_sb, T_sd));
+        % append current pose to output trajectory
+        outTrajectory(:, :, idxWaypoint) = T_sb;
+        % append articulation vector to output
+        articulations(:, idxWaypoint) = articulation;
+        if diagnosticMode
+            iterationsPerWaypoint(idxWaypoint) = numIterations;
+        end
+    end
+    if diagnosticMode
+        % store the number of solver iterations that were required to
+        % approach a waypoint in the base workspace
+        assignin("base", "iterationsPerWaypointBFGS", iterationsPerWaypoint);
+        % store the final error in the base workspace
+        assignin("base", "finalErrorBFGS", currDistance);
+    end
+end
+
+
+%% aditional function definitions
+% compute error twist in body (=TCP frame)
+function err = errorTwist(Ta, Tb)
+  % pose delta as seen from tangent at point a
+  deltaA =  logm(Ta \ Tb);
+  err_tvec = deltaA(1:3, 4); % tangent translation vector
+  w_x = deltaA(1:3, 1:3); % tangent rotation vector
+  err_rvec = [w_x(3,2); w_x(1,3); w_x(2,1)];
+  % weighted error vector in twist coordinates at the local
+  % tangent-space
+  err = [err_tvec; err_rvec];
+end
