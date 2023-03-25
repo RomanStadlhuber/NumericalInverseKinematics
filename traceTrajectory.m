1f0d7863b5f73f64733004fffa58955f94a0f3b4
Roman Stadlhuber <roman.stadlhuber@gmail.com>
2023-03-25 20:47:38 +0100

    update trajectory tracer fn increase damipng factor perform rank check and use regular GN in full rank scenario the results outperform bofh GN and BFGS in 7DOF Panda benchmark

--- traceTrajectory.m
+++ traceTrajectory.m
@@ -94,10 +94,16 @@ end
 
 % iteration step of damped least squares IK algorithm
 function deltaArticulation = iterateIK(robot, articulation, tcpName, targetPose)
-    l = 0.0125;
+    l = 0.125;
     tcpPose = getTransform(robot, articulation, tcpName);
     localError = errorTwist(tcpPose, targetPose);
     globalError = adjointSE3(tcpPose) * localError;
     J = spaceJacobian(robot, articulation);
-    deltaArticulation = J.' / (J * J.' + l^2 * eye(6)) * globalError;
+    fullRank = min(size(J));
+    if rank(J, 5e-3) == fullRank
+        A = pinv(J);
+    else
+        A = J.' / (J * J.' + l^2 * eye(6));
+    end
+    deltaArticulation = A * globalError;
 end
